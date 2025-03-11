21eaaa2a4d64ef020ca31e3c07774367f64ca573
witch250 <1641685226@qq.com>
2025-03-11 20:04:07 +0800

    加入（）还未测试

--- main.py
+++ main.py
@@ -12,35 +12,50 @@ def createsign():
     sign=['+','-','*','/']
     return random.choice(sign)
 
-def CreateQuestion(r,n):    #r是最大数字，n是算式数量，暂不考虑分数和括号
+def CreateQuestion(r):    #r是最大数字,暂不考虑分数和括号
     count=createnumber(2,4)     #算式中项的个数
     string=''                   #将算式保存在字符串中
     sign='+'
+    wait=0
+    kuohao=0
     for j in range(1,count+1):  #创建第一项到最后一项
+        if(count!=2 and j!=count):
+            if(createnumber(0,1)==1):
+                string+='('
+                kuohao+=1
         num=createnumber(0,r)
         if sign and sign=='/' and num==0:
             num=createnumber(1,r)
+        wait+=1
+        if(wait!=0):
+            if(createnumber(0,1)==1):
+                string+=')'
+                kuohao-=1
         num=str(num)
         string+=num
         if(j==count):   #不要生成过多的符号
             break
         sign=createsign()
         string+=sign
+    while kuohao!=0:
+        string+=')'
+        kuohao-=1
     return string
 
 def CreateQuestions(r,n):   #r是最大数字，n是算式数量
     Questions=[]
     for i in range(n):
-        q=CreateQuestion(r,n)
+        q=CreateQuestion(r)
         Questions.append(q)
     return Questions
 
-def CalculateResult(Question):#Question是字符串
+def CalculateResult(Question,count):#Question是字符串,count循环该函数，记录当前应该是字符串中第几个元素
     listnum=[]
     listsign=[]
     for elem in Question:  #放数字，乘除号先算
         if(elem=='0' or elem=='1' or elem=='2' or elem=='3' or elem=='4' or elem=='5' or elem=='6' or elem=='7' or elem=='8' or elem=='9'):
             listnum.append(elem)
+            count+=1
             if listsign:
                 if(listsign[-1]=='*'):
                     num1=eval(listnum.pop())
@@ -56,8 +71,17 @@ def CalculateResult(Question):#Question是字符串
                     listnum.append(str(num))
         if(elem=='+'or elem=='-' or elem=='*'or elem=='/'):
             listsign.append(elem)
+            count+=1
         if(elem=='('):
+            count+=1
             Q=[]
+            for i in Question[count:]:
+                Q.append(i)
+                if(elem==')'):
+                    count+=1
+                    break
+            result=CalculateResult(Q,count)
+            listnum.append(result)
 
     flag=True
     while(len(listnum)!=1): #加减法
@@ -85,7 +109,7 @@ def CalculateResult(Question):#Question是字符串
 def CalculateResults(Questions):
     Results=[]
     for i in Questions:
-        result=CalculateResult(i)
+        result=CalculateResult(i,0)
         Results.append(result)
     return Results
 
