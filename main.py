87c11f291cfd90ae056ca1297103463241bcc8f2
witch250 <1641685226@qq.com>
2025-03-12 23:04:50 +0800

    生成分数

--- main.py
+++ main.py
@@ -12,6 +12,27 @@ def createsign():
     sign=['+','-','*','/']
     return random.choice(sign)
 
+def createFenShu(a,b):
+    string=''
+    numUP=1
+    numDOWN=1
+    while numUP>=numDOWN:
+        numUP=createnumber(1,b)
+        numDOWN=createnumber(2,b)
+    numADD=createnumber(1,b)
+    string+='('+'A'+str(numADD)+'+'+str(numUP)+'/'+str(numDOWN)+')'
+    return string
+
+def CutA(Question):     #展示完了就删除A
+    L=[]
+    for i in Question:  
+        if(i!='A'):
+            L.append(i)
+    string=''
+    for i in L:
+        string+=i
+    return string
+
 def CreateQuestion(r):    #r是最大数字,暂不考虑分数和括号
     count=createnumber(2,4)     #算式中项的个数
     string=''                   #将算式保存在字符串中
@@ -19,15 +40,18 @@ def CreateQuestion(r):    #r是最大数字,暂不考虑分数和括号
     kuohao=0                    #括号个数
     flag=True                   #当前轮是否有'('生成
     for j in range(1,count+1):  #创建第一项到最后一项
-        if( count!=2 and j!=count): #不在仅有两项时，以及最后一项生成左括号
+        if( count!=2 and j!=count): #不在仅有两项时，以及最后一项生成左括号,两个左括号没法相邻
             if(createnumber(0,1)==1):   #50%
                 string+='('
                 kuohao+=1
                 flag=True
-        num=createnumber(0,r)
-        if sign and sign=='/' and num==0:   #不要1/0
-            num=createnumber(1,r)
-        num=str(num)
+        if createnumber(0,3)!=0:
+            num=createnumber(0,r)
+            if sign and sign=='/' and num==0:   #不要1/0
+                num=createnumber(1,r)
+            num=str(num)
+        else:
+            num=createFenShu(0,r)
         string+=num
         if(kuohao!=0 and flag==False): #不要在生成'('的时候也生成')',否则(6)
             if(createnumber(0,1)==1):
@@ -46,7 +70,32 @@ def CreateQuestion(r):    #r是最大数字,暂不考虑分数和括号
 
 def CutKuoHao(q):   #删除首个括号，使它更加简洁
     if(q[0]=='('and q[-1]==')'):
-        kuohao=0
+        Safe=[]
+        L=[]
+        pos=0
+        for i in q:
+            pos+=1
+            if(i=='('or i==')'):
+                if(i=='('):
+                    Safe.append(False)
+                    L.append(i)
+                elif(i==')'):
+                    for n in range(len(Safe)-1,-1,-1):
+                        if(Safe[n]==False):
+                            Safe[n]=True
+                            break               
+            if(pos==len(q)-1 and Safe[0]==False):
+                Q=[]
+                string=''
+                for j in q:
+                    Q.append(j)
+                del Q[0]
+                del Q[-1]
+                for i in Q:
+                    string+=i
+                q=string
+                break
+        '''
         for i in q[-2::-1]: #从倒数第二开始，倒着每次进1
             if i=='(':  #说明如果我切了，末尾会剩下'('
                 return q
@@ -60,7 +109,8 @@ def CutKuoHao(q):   #删除首个括号，使它更加简洁
                 for i in Q:
                     string+=i
                 q=string
-                break
+                break'
+            '''
     else:
         pass
     return q
@@ -69,13 +119,14 @@ def CreateQuestions(r,n):   #r是最大数字，n是算式数量
     Questions=[]
     for i in range(n):
         q=CreateQuestion(r)
+        print(q)
+        q=CutA(q)
         #print(q)
         q=CutKuoHao(q)
         #print(q)
         Questions.append(q)
     return Questions
   
-
 def ReversePolish(i):
     S=[]    #存放+-*/(
     L=[]    #存放数字+-*/,并且是结果
@@ -152,10 +203,10 @@ def CalculateResults(Questions):
     return Results
 
 if __name__=='__main__':
-    Questions=CreateQuestions(10-1,10)  #创建0~(10-1)范围内的10条式子
+    Questions=CreateQuestions(10-1,5)  #创建0~(10-1)范围内的5条式子
     
     print(Questions)
-    Results=CalculateResults(Questions) #计算存储式子的列表
-    print(Results)
+    #Results=CalculateResults(Questions) #计算存储式子的列表
+    #print(Results)
         
 
