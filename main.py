943749adbf5aa9b4ac44dc1f752abe6701b64a22
witch250 <1641685226@qq.com>
2025-03-15 16:47:25 +0800

    命令行

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 from write import *
 from calculator import *
+import sys
 import os
+#py main.py -n 12 -r 10
 if __name__=='__main__':
     try:
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
-        N=10
-        r=10             #r以内，不包括r
+        put=sys.argv
+        if len(put)>1: #调用cmd
+            try:
+                if put[1]=='' or put[2]=='' or put[3]=='' or put[4]=='':
+                    pass
+                elif put[1]=='-n' and put[3]=='-r':
+                    if(int(put[2])<=1 or int(put[4])<=1):
+                        print("-n 请输入大于0的数")
+                    elif(int(put[4])>10):
+                        print("-r 请输入1-10")
+                    else:
+                        N=int(put[2])
+                        r=int(put[4])
+                elif put[1]=='-e' and put[3]=='-a':
+                    exercisespath=put[2]
+                    youranswerspath=put[4]
+                else:
+                    print("输入有误")
+            except IndexError:
+                raise IndexError("输入过少") 
+        if len(put)==1: #脚本内参数
+            N=10
+            r=10             #r以内，不包括r
+            exercisespath="Exercises.txt"
+            youranswerspath="YourAnswers.txt"
         Questions=[]
         Results=[]
         i=1
@@ -34,4 +59,6 @@ if __name__=='__main__':
         print(e)
     except FileNotFoundError as e:
         print(e)
-
+    except IndexError as e:
+        print(e)
+    os.system('pause')
